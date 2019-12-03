c7db73f8a24ca7c4344c8f104b77439362f7068f
Alexander Bispo <alexbispo173@gmail.com>
2019-12-03 00:29:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
 class CreateCostumeStores <  ActiveRecord::Migration[4.2]
   
   def change
-    create_table :costume_stores do |x|  
+    create_table :costumeStores do |x|  
       x.string :name 
       x.string :location
       x.integer :number_of_costumes
